fd3372cd29b9e8a75d60950de0149833e2932959
Hansen Qian <Hansenq@gmail.com>
2013-02-17 05:23:55 -0500

    changed index.js to include logic!

--- javascripts/index.js
+++ javascripts/index.js
@@ -6,28 +6,83 @@ var cahPhrases = [];
 var cahPrompts = [];
 var cahPromptsPointer = 0;
 
-	document.write("Reading external javascript file succeess!");
+// Device identification information
+//var devNum;		// ie. Player _
+var time = (new Date()).getTime();
+var isJudge = false;
+var pickedCard = false;
+var hand = [];
 
+// Useful constants
+var url = 'https://wpcah.firebaseio.com';
 
-	// Executes on initialization (opening app)
+// Executes on initialization (opening app)
+var masterFB = new Firebase(url);
 
+// Adds this child to the online firebase, in order of players there
+var players = new Firebase(url + '/players');
+var playerRef = players.child('connected').push(time);
+playerRef.onDisconnect().remove();
+// Make players wait until a round changes
 
-	var fb = new Firebase('https://wpcah.firebaseio.com/');
+var judgePlayer = new Firebase(url + '/players/judge');
+judgePlayer.on('child_changed', function(snapshot) {
+	if (snapshot.val() === time) {
+		isJudge = true;
+	}
+});
 
-	// Testing whether or not firebase works! remove once working
-	fb.push({
-	    name: 'Hello world!',
-	    text: 'lorem ipsum!'
+var temp = 0;
+function getTotNumPlayers() {
+	players.once('connected', function (snapshot) {
+		temp = snapshot.numChildren();
 	});
+	return temp;
+}
+
+function changeToJudge() {
+	// Hide buttons
+}
+
+function changeFromJudge() {
+	// Display buttons
+}
 
 
+// Rooms contain the question, participating players, and all of the users!
+// Watches round for a creation of a new round!
+var rounds = new Firebase(url + '/round');
+var currRound;
+rounds.on('child_added', function (snapshot) {
+	if (isJudge) {
+		changeToJudge();
+	} else {
+		pickedCard = false;
+		currRound = snapshot.val();
+		startRound(currRound.questionPhrase);
+	}
+	// Display questionPhrase on the main page
+	// Allow user to press buttons?
+});
+// When someone new picks their word
+// For the judge: displays responses
+// For the player: does nothing
+var responses = [];
+currRound.on('child_added', function (snapshot) {
+	if (isJudge) {
+		responses[responses.length] = snapshot.val();
+	}
+});
 
-/*fb.push({
-	name: deviceName,
-	guid: deviceGuid
-});*/
 
-
+// When it presses 
+$('button').click(function () {
+	if (!isJudge) {
+		currRound.child('responses').push(RESPONSE);
+	} else {
+		pickedCard = true;
+	}
+});
 
 
 // callback function when some button is pressed
