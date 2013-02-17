33cc27d914d2301e243f618b50a2a7fa33fc7e49
Hansen Qian <Hansenq@gmail.com>
2013-02-17 02:39:25 -0500

    Added index.js

---
+++ javascripts/index.js
@@ -0,0 +1,32 @@
+// JavaScript source code
+
+
+// Read in CAH phrases, and store them in array
+var cahPhrases = [];
+var cahPrompts = [];
+var cahPromptsPointer = 0;
+
+
+
+// Executes on initialization (opening app)
+
+
+var firebase = new Firebase('https://wpcah.firebaseio.com/');
+
+// Testing whether or not firebase works! remove once working
+firebase.push({
+    name: 'Hello world!',
+    text: 'lorem ipsum!'
+});
+
+
+
+/*firebase.push({
+	name: deviceName,
+	guid: deviceGuid
+});*/
+
+
+
+
+// callback function when some button is pressed
