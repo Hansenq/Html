01440e602d108814b9d7457cfb4918f7f1e6b68a
Hansen Qian <Hansenq@gmail.com>
2013-02-17 03:37:05 -0500

    Got external javascripts working

--- javascripts/index.js
+++ javascripts/index.js
@@ -6,22 +6,23 @@ var cahPhrases = [];
 var cahPrompts = [];
 var cahPromptsPointer = 0;
 
+	document.write("Reading external javascript file succeess!");
 
 
-// Executes on initialization (opening app)
+	// Executes on initialization (opening app)
 
 
-var firebase = new Firebase('https://wpcah.firebaseio.com');
+	var fb = new Firebase('https://wpcah.firebaseio.com/');
 
-// Testing whether or not firebase works! remove once working
-firebase.push({
-    name: 'Hello world!',
-    text: 'lorem ipsum!'
-});
+	// Testing whether or not firebase works! remove once working
+	fb.push({
+	    name: 'Hello world!',
+	    text: 'lorem ipsum!'
+	});
 
 
 
-/*firebase.push({
+/*fb.push({
 	name: deviceName,
 	guid: deviceGuid
 });*/
